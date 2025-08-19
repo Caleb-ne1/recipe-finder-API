3015193c9e075334f4e89e889203c93f89883184
Caleb-ne1 <kemboicaleb19@gmail.com>
2025-08-19 14:07:34 +0300

    intial commit

---
+++ src/main/java/com/recipe_api/recipe_finder_api/RecipeFinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.recipe_api.recipe_finder_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeFinderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeFinderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipe_api/recipe_finder_api/RecipeFinderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipe_api.recipe_finder_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeFinderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
